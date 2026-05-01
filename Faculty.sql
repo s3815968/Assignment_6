efedb6db103ed231914aba3c8bb8f52e40450396
s3815968 <law340617@gmail.com>
2026-05-01 12:45:30 -0400

    Create Faculty.sql

---
+++ Faculty.sql
@@ -0,0 +1,8 @@
+USE FacultyDB;
+
+CREATE TABLE Faculty
+     (Faculty_ID VARCHAR(2),
+     LastName VARCHAR(20),
+     FirstName VARCHAR(20),
+     Department VARCHAR(20),
+     Campus VARCHAR(10));
